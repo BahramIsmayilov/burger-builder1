96d42941fe49a2975815c61c9393e92b80ba2353
Bahram Ismayilov <bahram.ismayilov@hotmail.com>
2020-06-01 19:21:08 +0400

    module.css file

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxiliary";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>hello</div>
+        <div>name: Mehman</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
